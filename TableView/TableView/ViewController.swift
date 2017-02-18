ff02e8b9a5c17850ff85e7ac506f33573c74812e
WluckyW <1536597545@qq.com>
2017-02-18 17:03:02 +0800

    tableview

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cellOne")
+        if cell == nil {
+            cell = UITableViewCell.init(style: .Plain, reuseIdentifier: "cellOne")
+        }
+        return cell!;
+        
+    }
 }
 
